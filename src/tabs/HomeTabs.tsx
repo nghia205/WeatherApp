f047e9346371e29dd379fa17200081fbbf8d6fd2
nghia <dtn04999@gmail.com>
2026-04-14 10:37:54 +0700

    style(navigation): sync Tab Navigator colors with active Paper theme

--- src/tabs/HomeTabs.tsx
+++ src/tabs/HomeTabs.tsx
@@ -2,13 +2,24 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
 import ListsScreen from '../screens/ListsScreen';
 import { House, List } from 'lucide-react-native';
+import { useTheme } from 'react-native-paper';
 
 const Tab = createBottomTabNavigator();
 
 function HomeTabs() {
+  const theme = useTheme();
+
   return (
     <Tab.Navigator
-      screenOptions={{ headerShown: false }}
+      screenOptions={{ 
+        headerShown: false,
+        tabBarStyle: { 
+          backgroundColor: theme.colors.elevation.level2,
+          borderTopWidth: 0,
+        },
+        tabBarActiveTintColor: theme.colors.primary,
+        tabBarInactiveTintColor: theme.colors.onSurfaceVariant,
+      }}
       initialRouteName="TabHome"
     >
       <Tab.Screen
