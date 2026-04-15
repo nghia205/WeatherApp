fd9863934823af9f8f80e647ef787d7a0eeeaabe
nghia <dtn04999@gmail.com>
2026-04-15 09:32:41 +0700

    build(android): bypass SSL certificate verification for local development

--- android/app/src/main/java/com/myawesomeproject/MainApplication.kt
+++ android/app/src/main/java/com/myawesomeproject/MainApplication.kt
@@ -10,6 +10,16 @@ import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 
+// Thư viện cần thiết cho việc Bypass SSL
+import com.facebook.react.modules.network.OkHttpClientProvider
+import com.facebook.react.modules.network.OkHttpClientFactory
+import okhttp3.OkHttpClient
+import java.security.cert.X509Certificate
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
+import java.security.SecureRandom
+
 class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost =
@@ -33,6 +43,34 @@ class MainApplication : Application(), ReactApplication {
 
   override fun onCreate() {
     super.onCreate()
+    
+    // Khởi tạo React Native chuẩn của bản 0.81
     loadReactNative(this)
+
+    // BẮT ĐẦU CODE BYPASS SSL (ÁP DỤNG CHO CẢ DEV VÀ BUILD APK)
+    try {
+        val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
+            override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
+            override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {}
+            override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
+        })
+
+        val sslContext = SSLContext.getInstance("SSL")
+        sslContext.init(null, trustAllCerts, SecureRandom())
+        val sslSocketFactory = sslContext.socketFactory
+
+        OkHttpClientProvider.setOkHttpClientFactory(object : OkHttpClientFactory {
+            override fun createNewNetworkModuleClient(): OkHttpClient {
+                // Giữ lại cấu hình Cookie/Session của React Native
+                return OkHttpClientProvider.createClientBuilder()
+                    .sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
+                    .hostnameVerifier { _, _ -> true }
+                    .build()
+            }
+        })
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+    // KẾT THÚC CODE BYPASS SSL
   }
 }
