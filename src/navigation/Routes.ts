cb1cac3f33299ad7823ad4cc3aebd6b04efd39d9
nghia <dtn04999@gmail.com>
2026-04-15 09:23:00 +0700

    feat(navigation): update routing configuration

--- src/navigation/Routes.ts
+++ src/navigation/Routes.ts
@@ -2,7 +2,7 @@ import LoginScreen from '../screens/LoginScreen';
 import HomeTabs from '../tabs/HomeTabs';
 
 const Routes = [
-  { name: 'Home', component: HomeTabs, auth: false },
+  { name: 'Home', component: HomeTabs, auth: true },
   { name: 'Login', component: LoginScreen, auth: false },
 ];
 
