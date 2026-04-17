b393412d6f191fff43480aba59668ae65e30a1c2
nghia <dtn04999@gmail.com>
2026-04-17 11:00:52 +0700

    test: support ESM React Native dependencies in Jest

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   preset: 'react-native',
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native|@react-native-community|@react-native-async-storage|react-native-paper|react-native-safe-area-context|react-native-screens|react-native-toast-message|@react-navigation)/)',
+  ],
 };
