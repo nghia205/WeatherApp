6e918849924b2e58e814083286331b849c8b0926
nghia <dtn04999@gmail.com>
2026-04-20 14:53:31 +0700

    feat: add image picker for job image upload

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: 'react-native',
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native|@react-native-community|@react-native-async-storage|react-native-paper|react-native-safe-area-context|react-native-screens|react-native-toast-message|@react-navigation)/)',
+    'node_modules/(?!((jest-)?react-native|@react-native|@react-native-community|@react-native-async-storage|react-native-image-picker|react-native-paper|react-native-safe-area-context|react-native-screens|react-native-toast-message|@react-navigation)/)',
   ],
 };
